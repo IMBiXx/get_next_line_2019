a5ec986ab8dcaf26162816a9c305e490cf941848
Valentin LECART <valecart@e2r9p10.42.fr>
2019-04-12 20:25:11 +0200

    finish

--- get_next_line.c
+++ get_next_line.c
@@ -6,46 +6,50 @@
 /*   By: valecart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 14:27:12 by valecart          #+#    #+#             */
-/*   Updated: 2019/04/11 11:36:10 by valecart         ###   ########.fr       */
+/*   Updated: 2019/04/12 16:20:09 by valecart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "get_next_line.h"
 
-static char	*join_next_line(int const fd, char *str, int *rd)
+static char		*join_next_line(int const fd, char *str, int *rd)
 {
 	char	buff[BUFF_SIZE + 1];
 	char	*tmp;
 
 	tmp = NULL;
 	*rd = read(fd, buff, BUFF_SIZE);
-	if (*rd < BUFF_SIZE && buff[*rd - 1] != '\n')
-	{
-		buff[*rd] = '\n';
-		buff[*rd + 1] = '\0';
-	}
-	else
-		buff[*rd] = '\0';
+	if (*rd == -1)
+		return (NULL);
+	buff[*rd] = '\0';
 	tmp = str;
 	str = ft_strjoin(str, buff);
 	ft_strdel(&tmp);
 	return (str);
 }
 
-#include <stdio.h>
-int			get_next_line(int const fd, char **line)
+int				get_eof_line(char **str, char ***line)
+{
+	if ((ft_strlen(*str) > 1) && ft_strrchr(*str, '\0'))
+	{
+		**line = ft_strdup(*str);
+		ft_strdel(str);
+		return (1);
+	}
+	return (0);
+}
+
+int				get_next_line(int const fd, char **line)
 {
 	int				rd;
-	char		*tmp;
+	char			*tmp;
 	static char		*str = NULL;
 
 	if (!line || fd < 0)
 		return (-1);
 	rd = 1;
-	tmp = NULL;
-	if (!str)
-		str = ft_strnew(0);
+	!str ? str = ft_strnew(0) : 0;
 	while (rd > 0)
 	{
 		if ((tmp = ft_strchr(str, '\n')))
@@ -56,15 +60,11 @@ int			get_next_line(int const fd, char **line)
 			str = ft_memmove(str, tmp, ft_strlen(tmp) + 1);
 			return (1);
 		}
-		str = join_next_line(fd, str, &rd);
+		if (!(str = join_next_line(fd, str, &rd)))
+			return (-1);
 	}
-	if (ft_strlen(str) > 1 && ft_strrchr(str, '\0'))
-	{
-		*line = ft_strdup(str);
-		ft_strdel(&str);
+	if (get_eof_line(&str, &line) == 1)
 		return (1);
-	}
-	if (rd == 0)
-		*line = ft_strnew(0);
+	rd == 0 ? *line = ft_strnew(0) : 0;
 	return (rd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: Valentin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 20:24:24 by Valentin          #+#    #+#             */
-/*   Updated: 2019/04/11 12:04:54 by valecart         ###   ########.fr       */
+/*   Updated: 2019/04/12 16:22:14 by valecart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
-# define BUFF_SIZE 10
+# define BUFF_SIZE 1
+
 int		get_next_line(const int fd, char **line);
 
 #endif
